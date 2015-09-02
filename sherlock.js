bca381b94ce6560703556278b45ec76516c06185
juantejedavz <juan.tejedavz@udlap.mx>
2015-09-02 10:24:53 -0500

    Tarea2 de JuanTejeda to forked repository

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,73 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+// Obtener número de casos introducidos
+cases= _INPUT.charAt(0);
+console.log("Casos: "+cases);
+		
+		x=[];
+		y=[];
+			// Inicializar arreglos en 0
+				for(n=0;n<cases;n++){
+					x[n]=0;y[n]=0;
+				}
+			
+		/* Busqueda de valores en _INPUT para ser almacenados en variables
+		por lectura de caracteres
+		*/	
+		i=0;
+		j=0;	
+	while(j<cases){
+		if(_INPUT.charAt(i)=='\n'){
+			i++;
+			while(_INPUT.charAt(i)!=" "){
+			 x[j]=x[j]+_INPUT.charAt(i);
+			 i++;
+			}
+			i++;
+			while(_INPUT.charAt(i)!="\n" && i<=_INPUT.length-1){
+			 y[j]=y[j]+_INPUT.charAt(i);
+			 i++;
+			}
+			j++;
+			i--;
+		}
+		i++;
+	}
+
+// Calculo de casos
+calcular(x,y,cases);
+
+// Funcion de busqueda de caso de Sherlock
+function calcular(x,y,cases){
+
+	// Ciclo for para n casos
+	for(i=0;i<cases;i++){
+		k=0;
+		num=x[i];
+		raices=[];
+
+		//Ciclo de busqueda en forma ascendente de valores
+		while(num<=y[i]){
+			
+				/* Chequeo de numero Square Integer  
+				console.log("Checking root of "+num);
+				*/
+			if(Math.sqrt(num)%2==0||Math.sqrt(num)%2==1){
+				raices[k]=num;
+				k++;
+				//console.log("S.I. found: "+num);
+			}
+		 num++;
+		}
+			console.log(raices.length);
+
+			//Impresion de arreglo de Square Integers encontrados
+			/* if(raices.length!=0){
+				//console.log("Square Integers for case "+(i+1)+": "+raices);
+			}
+			else{
+			//console.log("Square Integers for case "+(i+1)+": none");
+			} */
+	}
+}
